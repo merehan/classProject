0770f9db9f8f6466ac97ff09b1f50f2b2ee6aef1
Merehan Elamrosy <merehan.suhiel@gmail.com>
2020-03-17 21:35:03 +0100

    new changes to framer .. merehan Suhiel

---
+++ code/canvas.tsx
@@ -0,0 +1,4 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Settings_card = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Profile_Picture?:string,you_can_edit_your_profile_picture?:string}>(canvas, "id_SZKFShWKw", {Profile_Picture:"string",you_can_edit_your_profile_picture:"string"}, 375,85);
